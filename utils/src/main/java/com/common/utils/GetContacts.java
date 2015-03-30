fbff1b97896cc92e42471d3addd81bd5120d388f
Mayank <mayankdudhatra01@gmail.com>
2015-03-30 14:40:58 +0530

    Classes Change

--- utils/src/main/java/com/common/utils/GetContacts.java
+++ utils/src/main/java/com/common/utils/GetContacts.java
@@ -11,6 +11,7 @@ public class GetContacts extends Activity {
     /**
      * Called when the activity is first created.
      */
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
