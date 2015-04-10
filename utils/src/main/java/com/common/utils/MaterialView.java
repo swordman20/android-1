f90c86611fd2bed5c04ee655a705fa774182357b
Mayank <mayankdudhatra01@gmail.com>
2015-04-10 18:31:30 +0530

    Snippet
    Add Snippet

---
+++ utils/src/main/java/com/common/utils/MaterialView.java
@@ -0,0 +1,17 @@
+package com.common.utils;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Mayank on 4/10/2015.
+ */
+public class MaterialView extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_material);
+
+    }
+}
